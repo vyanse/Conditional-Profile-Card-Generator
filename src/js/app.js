e860da7fdb112f0f065a0c8630fc7584287b1b20
vyanse <valentinayulloa@gmail.com>
2022-07-20 22:50:29 +0000

    entrega

--- src/js/app.js
+++ src/js/app.js
@@ -33,10 +33,17 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
+          <h1>${variables.name ? variables.name : ""} 
+          ${variables.lastname ? variables.lastname : ""} </h1>
+          <h2>${variables.role ? variables.role : ""}  </h2>
+          <h3>${variables.country ? variables.country : ""},${
+    variables.city ? variables.city : ""
+  } 
+          ${variables.twitter ? variables.twitter : ""}
+          ${variables.github ? variables.github : ""} 
+          ${variables.linkedin ? variables.linkedin : ""} 
+          ${variables.instagram ? variables.instagram : ""} </h3>
+          <ul class=" ${variables.socialMediaPosition}">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
