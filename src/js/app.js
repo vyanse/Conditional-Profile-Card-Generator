677c8d1c69ff2749a4bfa6b126608c56934af4cd
vyanse <valentinayulloa@gmail.com>
2022-07-25 22:53:32 +0000

    correcion1

--- src/js/app.js
+++ src/js/app.js
@@ -33,24 +33,29 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>${variables.name ? variables.name : ""} 
-          ${variables.lastname ? variables.lastname : ""} </h1>
+          <h1> ${variables.name ? variables.name : ""} ${
+    variables.lastname ? variables.lastname : " "
+  }
+          </h1>
           <h2>${variables.role ? variables.role : ""}  </h2>
-          <h3>${variables.country ? variables.country : ""},${
-    variables.city ? variables.city : ""
-  } 
-          ${variables.twitter ? variables.twitter : ""}
-          ${variables.github ? variables.github : ""} 
-          ${variables.linkedin ? variables.linkedin : ""} 
-          ${variables.instagram ? variables.instagram : ""} </h3>
+          <h3>${variables.country ? variables.country : ""},
+          ${variables.city ? variables.city : ""} </h3>
           <ul class=" ${variables.socialMediaPosition}">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
-          </ul>
-        </div>
-    `;
+          <li><a href="https://twitter.com/${
+            variables.twitter ? variables.twitter : " "
+          }"><i class="fab fa-twitter"></i></a></li>
+       <li><a href="https://github.com/${
+         variables.github ? variables.github : " "
+       }"><i class="fab fa-github"></i></a></li>
+          <li><a href="https://www.linkedin.com/${
+            variables.linkedin ? variables.linkedin : " "
+          }"><i class="fab fa-linkedin"></i></a></li>
+          <li><a href="https://instagram.com/${
+            variables.instagram ? variables.inst : " "
+          }"><i class="fab fa-instagram"></i></a></li>
+        </ul>
+      </div>
+  `;
 }
 
 /**
